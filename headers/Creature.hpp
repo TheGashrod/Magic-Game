bbee5b130a74ac3188a033b28fe91c74a48c08af
Gael-Bernard <gael.berbere@gmail.com>
2022-01-18 18:57:08 +0100

    Added Creature

--- headers/Creature.hpp
+++ headers/Creature.hpp
@@ -4,16 +4,60 @@
 
 
 
+typedef unsigned char stat;
 class Creature : public Card {
 
+	// Members :
 	private :
+		std::tuple<int, std::list<Color>> c_cost;
+		stat c_power;
+		stat c_powerDef;
+		stat c_toughness;
+		stat c_toughnessDef;
 
-	public :
-		// TODO : Pourquoi un int si on a aussi une couleur Any ? Il faut choisir
-		std::tuple<int, std::list<Color>> cost;
+	//Constructors :
+	public:
+		Creature (string name, std::list<Color> colors, bool isEngaged,
+			std::list<Color> colorCost, int anyCost,
+			stat power, stat powerDefault, stat toughness, stat toughnessDefault);
+		
+		~Creature();
 
-		//Constructors :
-		Creature (int, std::list<Color>);
-      ~Creature();
+
+	// Getters/setters
+	public:
+		std::tuple<int, std::list<Color>> getCost() const;
+		/**
+		 * @brief Get the Color cost of a creature
+		 * 
+		 * @return std::list<Color> one colour per defined necessary land to invoke
+		 */
+		std::list<Color> getColorCost() const;
+		/**
+		 * @brief Get the remaining cost of a creature, that can be any ground
+		 * 
+		 * @return int the number of necessary grounds
+		 */
+		int getAnyCost() const;
+		stat getPower() const;
+		stat getPowerDefault() const;
+		stat getToughness() const;
+		stat getToughnessDefault() const;
+
+		void setPowerDefault(stat val);
+		void setToughnessDefault(stat val);
+		void setPower(stat val);
+		void setToughness(stat val);
+		void usePower(stat amount);
+		void regainPower(stat amount);
+		void damageBy(stat hp);
+		void healBy(stat hp);
+		void resetStats();
+
+
+	// Methods :
+	public:
+		void dealDamageTo(Creature* c);
+		void receiveDamageFrom(Creature* c);
 
 };
--- src/Creature.cpp
+++ src/Creature.cpp
@@ -1,1 +1,50 @@
- 
+#include "../headers/Creature.hpp"
+
+#include <algorithm>
+
+using std::tuple;
+
+
+//Constructors :
+Creature::Creature (string name, std::list<Color> colors, bool isEngaged,
+std::list<Color> colorCost, int anyCost,
+stat power, stat powerDefault, stat toughness, stat toughnessDefault)
+: Card(name, colors, isEngaged),
+	c_cost( {anyCost, std::list<Color>(colorCost)} ),
+	c_power(power), c_powerDef(powerDefault), c_toughness(toughness), c_toughnessDef(toughnessDefault) { }
+
+Creature::~Creature() { };
+
+
+// Getters/setters :
+tuple<int, std::list<Color>> Creature::getCost() const { return tuple<int, std::list<Color>>(c_cost); }
+std::list<Color> Creature::getColorCost() const { return std::list<Color>(  std::get<1>(c_cost) ); }
+int Creature::getAnyCost() const { return std::get<0>(c_cost); }
+stat Creature::getPower() const { return c_power; }
+stat Creature::getPowerDefault() const { return c_powerDef; }
+stat Creature::getToughness() const { return c_toughness; }
+stat Creature::getToughnessDefault() const { return c_toughnessDef; }
+
+void Creature::setPowerDefault(stat val) { c_powerDef = val; }
+void Creature::setToughnessDefault(stat val) { c_toughnessDef = val; }
+void Creature::setPower(stat val) { c_power = val; }
+void Creature::setToughness(stat val) { c_toughness = val; }
+void Creature::usePower(stat amount) { c_power += amount; }
+void Creature::regainPower(stat amount) { c_power = std::max( (stat)0 , amount ); }
+void Creature::healBy(stat hp) { c_toughness += hp; }
+void Creature::damageBy(stat hp) { c_toughness = std::max( (stat)0 , hp ); }
+void Creature::resetStats() {
+	c_power = c_powerDef;
+	c_toughness = c_toughnessDef;
+}
+
+
+// Methods :
+void Creature::dealDamageTo(Creature* c) {
+	c->receiveDamageFrom(this);
+}
+void Creature::receiveDamageFrom(Creature* c) {
+	stat damage = c->getPower();
+	this->damageBy( damage );
+	c->usePower( damage );
+}
