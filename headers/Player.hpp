853b7890aba448f0e56aad4b287cf4b0c857239c
TheGashrod <simoelmawork@gmail.com>
2022-01-18 23:01:42 +0100

    Player

--- headers/Player.hpp
+++ headers/Player.hpp
@@ -1,2 +1,16 @@
 
 #define UNUSED(x) (void)(x) 
+
+
+class Player {
+
+public:
+	
+	string name;
+
+    string getName();
+
+    unsigned char getRemainingTurns();
+    void setName(string playerName);
+
+};
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,1 +1,14 @@
- 
+
+
+#include "Player.hpp"
+
+//Constructors :
+Player::Player (string name): p_name(name) { }
+
+Player::~Player() { };
+
+// Getters :
+string Player::getName() const { return p_name; }
+
+// setters :
+void Player::setName(string name) { p_name = name; }
